f8f68135ad893c9d3b6bd1763cfe621919e50c82
Arran France <arranf@gmail.com>
2019-02-07 00:20:37 +0000

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,18 @@
 #![no_main] // We don't have access to the C runtime library so main cannot be called 
 
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 // Entry point on Linux
 #[no_mangle] // Ensure the linker finds _start instead of a mangled name
 pub extern "C" fn _start() -> ! { //extern C defines the C calling convention
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Welcome to Ros");
     loop {}
 }
 
 // We need to define our own panic handler, Rust's panic handler is provided by std
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    // Replace this later
+    println!("{}", info);
     loop {}
 }
